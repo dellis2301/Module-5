14eed076b71fde90e7bac9119a0ef959ae36fee3
dellis2301 <dellis75@ivytech.edu>
2026-04-20 22:51:55 -0400

    Add password reset endpoint

---
+++ Insecure Design/7.py
@@ -0,0 +1,8 @@
+@app.route('/reset-password', methods=['POST'])
+def reset_password():
+    email = request.form['email']
+    new_password = request.form['new_password']
+    user = User.query.filter_by(email=email).first()
+    user.password = new_password
+    db.session.commit()
+    return 'Password reset'
