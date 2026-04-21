c9fea2b479c58110876794e5c77abcb98c68c9b3
dellis2301 <dellis75@ivytech.edu>
2026-04-20 22:50:13 -0400

    Implement user fetching route with NoSQL caution
    Added a route to fetch user data by username.

---
+++ Injection/6.js
@@ -0,0 +1,7 @@
+app.get('/user', (req, res) => {
+    // Directly trusting query parameters can lead to NoSQL injection
+    db.collection('users').findOne({ username: req.query.username }, (err, user) => {
+        if (err) throw err;
+        res.json(user);
+    });
+});
