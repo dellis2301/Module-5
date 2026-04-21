7326aefa881d4d859a161f8314fe9fa664bd5369
dellis2301 <dellis75@ivytech.edu>
2026-04-20 22:49:10 -0400

    Add user query execution in Injection/5.java

---
+++ Injection/5.java
@@ -0,0 +1,4 @@
+String username = request.getParameter("username");
+String query = "SELECT * FROM users WHERE username = '" + username + "'";
+Statement stmt = connection.createStatement();
+ResultSet rs = stmt.executeQuery(query);
