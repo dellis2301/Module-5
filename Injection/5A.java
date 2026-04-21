d168cbfa700c8c0f5ec9d3473d28140c3186bb12
dellis2301 <dellis75@ivytech.edu>
2026-04-20 22:49:40 -0400

    Add SQL query execution for user lookup

---
+++ Injection/5A.java
@@ -0,0 +1,4 @@
+String query = "SELECT * FROM users WHERE username = ?";
+PreparedStatement stmt = connection.prepareStatement(query);
+stmt.setString(1, username);
+ResultSet rs = stmt.executeQuery();
